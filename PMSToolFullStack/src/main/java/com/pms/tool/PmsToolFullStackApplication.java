a0cfc1a6767155cea7aec0d7b727c561c0b8e019
prakashj2ee20 <prakashj2ee20@gmail.com>
2020-07-14 08:12:09 +0530

    first commit

---
+++ PMSToolFullStack/src/main/java/com/pms/tool/PmsToolFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.pms.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsToolFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmsToolFullStackApplication.class, args);
+	}
+
+}
